1efa07e5d9c2ed2039ca00e8ad4bd68dd9f14149
quang.ho <quang.ho@auvietsoft.vn>
2020-08-03 10:37:14 +0700

    handle event has param

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+  }
+  
+  const setToZero = () => setCounter(0)
+
+  const getParam = (event, a)=>{
+    console.log(event)
+    console.log(a);
+  }
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
+
+      <button onClick={(event)=>getParam(event, 123)}>
+        param
+      </button>
+
+    </div>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
