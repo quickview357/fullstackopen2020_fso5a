0b3363a79a2aa5c40a56b1889a8a7bc931131a46
quang.ho <quang.ho@auvietsoft.vn>
2020-08-21 22:50:40 +0700

    5a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect  } from 'react'
 import noteService  from './services/note'
+import loginService from './services/login'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
@@ -9,6 +10,9 @@ const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   //const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
@@ -27,31 +31,63 @@ const App = (props) => {
       })
   }, [])
 
+  useEffect(()=>{
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  },[])
+
+
+
   console.log('render', notes.length, 'notes')
   const name = useFormInput('quang')
   const surname = useFormInput('ho')
 
-  const addNote = (event)=>{
-    event.preventDefault()    
+  // const addNote = (event)=>{
+  //   event.preventDefault()    
+  //   const noteObject = {
+  //     content: newNote,
+  //     date: new Date().toISOString(),
+  //     important: Math.random() < 0.5,
+  //     id: notes.length + 1,
+  //   }
+  //   noteService
+  //     .create(noteObject)
+  //     .then(returnedNote  => {
+  //       setNotes(notes.concat(returnedNote))
+  //       setNewNote('')
+  //     })
+  //     .catch(error => {
+  //       setErrorMessage('token error')
+  //       setTimeout(() => {
+  //         setErrorMessage(null)
+  //       }, 5000)
+  //     })          
+  // }
+  const addNote = async (event)=>{
+    event.preventDefault() 
+
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
       id: notes.length + 1,
     }
-    noteService
-      .create(noteObject)
-      .then(returnedNote  => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
-      .catch(error => {
-        console.log(error)
-        alert(
-          "Could not load data"
-        )
-      })
-          
+    try{
+      const returnedNote = await noteService.create(noteObject);
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    }
+    catch(ex){
+      console.log(ex)
+      setErrorMessage('token error')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }      
   }
   
   const handleNoteChange = (event)=> {
@@ -100,10 +136,70 @@ const App = (props) => {
       })   
   }
 
+  //
+  //login handle
+  //
+  const handleLogin = async (event)=>{
+    event.preventDefault();
+    try{
+      const user = await loginService.login({username, password})
+      //write token to local storage
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      //set token for note service 
+      noteService.setToken(user.token)
+      //set user info
+      setUser(user)
+      //reset
+      setUserName('')
+      setPassword('')
+    }
+    catch(ex){
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }      
+  }
+
+  //
+  //declare login form and add note form like as varible
+  //
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        User name: <input type="text" value={username} name="username" onChange={({target})=>setUserName(target.value)}></input>
+      </div>
+      <div>
+        Password: <input type="password" value={password} name="Password" onChange={(event)=>setPassword(event.target.value)}></input>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input onChange={handleNoteChange} value={newNote}/>
+      <button type="submit">Save</button>        
+    </form>
+  )
+  
+  //
+  //component return
+  //
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage}></Notification>
+      { 
+        user === null ?
+        loginForm():
+        <div>
+          <p>{user.username} - {user.name} has been login</p>
+          {noteForm()}
+        </div>        
+      }
       <ul>
         {notes.map(note => 
           <Note 
@@ -112,13 +208,7 @@ const App = (props) => {
             toggleImportance = {() => toggleImportance(note.id)}
             />
         )}
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input onChange={handleNoteChange} value={newNote}/>
-        <button type="submit">save</button>        
-      </form>  
-
+      </ul>    
 
       <br></br>
       <input {...name}/>
--- src/services/note.js
+++ src/services/note.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:5000/api/notes'
 //const baseUrl = '/api/notes'
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -14,9 +19,16 @@ const getAll = () => {
     return request.then(response => response.data.concat(nonExisting))
 }
 
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
+// const create = newObject => {
+//     const request = axios.post(baseUrl, newObject)
+//     return request.then(response => response.data)
+// }
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
 const update = (id, newObject) => {
@@ -27,5 +39,6 @@ const update = (id, newObject) => {
 export default { 
   getAll: getAll, 
   create: create, 
-  update: update 
+  update: update,
+  setToken 
 }
